ba064bf6a103465241a4cfed3576a02be9909877
Calvin <calvin.lieu@gmail.com>
2023-03-26 18:14:31 -0700

    added java swing flow layout manager

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,48 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import javax.swing.JButton;
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		//BorderLayout = A BorderLayout places components in five areas: north south west east center.
-						// all Extra space is placed in the center area.
-		
-		
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 500);
-		frame.setLayout(new BorderLayout(10, 10));
-		frame.setVisible(true);
-		
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
-		JPanel panel3 = new JPanel();
-		JPanel panel4 = new JPanel();
-		JPanel panel5 = new JPanel();
-		
-		panel1.setBackground(Color.red);
-		panel2.setBackground(Color.green);
-		panel3.setBackground(Color.yellow);
-		panel4.setBackground(Color.magenta);
-		panel5.setBackground(Color.blue);
-		
-		panel1.setPreferredSize(new Dimension(100, 100));
-		panel2.setPreferredSize(new Dimension(100, 100));
-		panel3.setPreferredSize(new Dimension(100, 100));
-		panel4.setPreferredSize(new Dimension(100, 100));
-		panel5.setPreferredSize(new Dimension(100, 100));
-		
-		//------------- sub panels =====================
-		
-		
-		JPanel panel6 = new JPanel();
-		JPanel panel7 = new JPanel();
-		JPanel panel8 = new JPanel();
-		JPanel panel9 = new JPanel();
-		JPanel panel10 = new JPanel();
-		
-		panel6.setBackground(Color.black);
-		panel7.setBackground(Color.darkGray);
-		panel8.setBackground(Color.gray);
-		panel9.setBackground(Color.lightGray);
-		panel10.setBackground(Color.white);
-		
-		panel5.setLayout(new BorderLayout());
-		
-		panel6.setPreferredSize(new Dimension(50, 50));
-		panel7.setPreferredSize(new Dimension(50, 50));
-		panel8.setPreferredSize(new Dimension(50, 50));
-		panel9.setPreferredSize(new Dimension(50, 50));
-		panel10.setPreferredSize(new Dimension(50, 50));
-		
-		panel5.add(panel6, BorderLayout.NORTH);
-		panel5.add(panel7, BorderLayout.SOUTH);
-		panel5.add(panel8, BorderLayout.WEST);
-		panel5.add(panel9, BorderLayout.EAST);
-		panel5.add(panel10, BorderLayout.CENTER);
-		
-		frame.add(panel1, BorderLayout.NORTH);
-		frame.add(panel2, BorderLayout.WEST);
-		frame.add(panel3, BorderLayout.EAST);
-		frame.add(panel4, BorderLayout.SOUTH);
-		frame.add(panel5, BorderLayout.CENTER);
-		
-		
-		
-		
-	}
 
+    public static void main(String[] args) {
+    	
+    	//Layout Manager = Defines the natural layout for components within a container
+    	
+    	// FlowLayout = places compopnents in a row, sized at their preferred size.
+    					//if the horizontal space in the container is too small,
+    					//the FlowLayout class uses the next available row.
+    	
+    	JFrame frame = new JFrame();
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(500, 500);
+    	frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+    	
+    	
+    	JPanel panel = new JPanel();
+    	panel.setPreferredSize(new Dimension(50,250));
+    	panel.setBackground(Color.lightGray);
+    	panel.setLayout(new FlowLayout());
+    	
+    	panel.add(new JButton("1"));
+    	panel.add(new JButton("2"));
+    	panel.add(new JButton("3"));
+    	panel.add(new JButton("4"));
+    	panel.add(new JButton("5"));
+    	panel.add(new JButton("6"));
+    	panel.add(new JButton("7"));
+    	panel.add(new JButton("8"));
+    	panel.add(new JButton("9"));
+    	
+    	frame.add(panel);
+    	frame.setVisible(true);
+    	
+        
+    };
+    
 }
+
+    
