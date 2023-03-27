a61c22055c09634ed69634d985a31aefb1c70331
Calvin <calvin.lieu@gmail.com>
2023-03-27 12:57:00 -0700

    added grid layout practice

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 public class Main {
 
@@ -15,31 +15,27 @@ public class Main {
     	// FlowLayout = places compopnents in a row, sized at their preferred size.
     					//if the horizontal space in the container is too small,
     					//the FlowLayout class uses the next available row.
+    	//GridLayout = places componenets in a grid of cells.
+    					//each component takes all the available space within its cell and each cell is the same size.
     	
     	JFrame frame = new JFrame();
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	frame.setSize(500, 500);
-    	frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+    	frame.setLayout(new GridLayout(3, 3, 10, 10));
+    	
+    	frame.add(new JButton("1"));
+    	frame.add(new JButton("2"));
+    	frame.add(new JButton("3"));
+    	frame.add(new JButton("4"));
+    	frame.add(new JButton("5"));
+    	frame.add(new JButton("6"));
+    	frame.add(new JButton("7"));
+    	frame.add(new JButton("8"));
+    	frame.add(new JButton("9"));
+    	frame.add(new JButton("10"));
     	
     	
-    	JPanel panel = new JPanel();
-    	panel.setPreferredSize(new Dimension(50,250));
-    	panel.setBackground(Color.lightGray);
-    	panel.setLayout(new FlowLayout());
-    	
-    	panel.add(new JButton("1"));
-    	panel.add(new JButton("2"));
-    	panel.add(new JButton("3"));
-    	panel.add(new JButton("4"));
-    	panel.add(new JButton("5"));
-    	panel.add(new JButton("6"));
-    	panel.add(new JButton("7"));
-    	panel.add(new JButton("8"));
-    	panel.add(new JButton("9"));
-    	
-    	frame.add(panel);
     	frame.setVisible(true);
-    	
         
     };
     
