b164d4d2075b2cbcf40cf211912c23496025006c
Calvin <calvin.lieu@gmail.com>
2023-03-28 14:06:24 -0700

    added new window practice

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-
-
-import javax.swing.JFrame;
-
-import java.awt.GridLayout;
-
-import javax.swing.JButton;
 public class Main {
 
 
@@ -18,24 +11,7 @@ public class Main {
     	//GridLayout = places componenets in a grid of cells.
     					//each component takes all the available space within its cell and each cell is the same size.
     	
-    	JFrame frame = new JFrame();
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setSize(500, 500);
-    	frame.setLayout(new GridLayout(3, 3, 10, 10));
-    	
-    	frame.add(new JButton("1"));
-    	frame.add(new JButton("2"));
-    	frame.add(new JButton("3"));
-    	frame.add(new JButton("4"));
-    	frame.add(new JButton("5"));
-    	frame.add(new JButton("6"));
-    	frame.add(new JButton("7"));
-    	frame.add(new JButton("8"));
-    	frame.add(new JButton("9"));
-    	frame.add(new JButton("10"));
-    	
-    	
-    	frame.setVisible(true);
+    	LaunchPage launchPage = new LaunchPage();
         
     };
     
